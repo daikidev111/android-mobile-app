06ed6a71a440ee3fe01a723e478a189838d34627
daikidev111 <dkub0001@student.monash.edu>
2023-05-12 03:05:40 +0800

    added eventMotion tasks@

--- app/src/main/java/com/example/android_app/bookManagementActivity.java
+++ app/src/main/java/com/example/android_app/bookManagementActivity.java
@@ -3,6 +3,7 @@ package com.example.android_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -17,6 +18,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -35,26 +37,19 @@ import java.util.StringTokenizer;
 public class bookManagementActivity extends AppCompatActivity {
 //    private final List<Book> bookCollection = new ArrayList<>();
     TextView editID, editTitle, editISBN, editAuthor, editDescription, editPrice;
+    ConstraintLayout motionLayout;
     FloatingActionButton fabAdd;
-
-//    ArrayAdapter bookAdapter;
-    ListView bookListView;
 
     Toolbar bookToolBar;
     DrawerLayout bookDrawer;
 
     NavigationView bookNavigation;
 
-//    ArrayList<Book> bookList = new ArrayList<Book>();
-
-//    RecyclerView recyclerView;
-//    RecyclerView.LayoutManager layoutManager;
-//    MyRecyclerViewAdapter adapter;
-
     // declare view model for book object
     private BookViewModel mBookViewModel;
 
-    public int count = 0;
+    // variables for keeping track of touch movement
+    private int x_start, y_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,15 +64,11 @@ public class bookManagementActivity extends AppCompatActivity {
         editAuthor = findViewById(R.id.editAuthor);
         editDescription = findViewById(R.id.editDescription);
         editPrice = findViewById(R.id.editPrice);
+        motionLayout = findViewById(R.id.motion_layout);
 
         /* Request permissions to access SMS */
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 0);
         registerReceiver(bookBroadCastReceiver, new IntentFilter(SMSReceiver.SMS_FILTER));
-
-        // this creates a list view with an adapter
-//        bookListView = findViewById(R.id.bookListView);
-//        bookAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, bookList);
-//        bookListView.setAdapter(bookAdapter);
 
         // creation of fab button
         fabAdd = findViewById(R.id.fab_add);
@@ -105,15 +96,6 @@ public class bookManagementActivity extends AppCompatActivity {
         // Fragment
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_book_frame, new BookRecyclerViewFragment()).commit();
 
-//        // Recycler View
-//        recyclerView = findViewById(R.id.rv);
-//
-//        layoutManager = new LinearLayoutManager(this);  //A RecyclerView.LayoutManager implementation which provides similar functionality to ListView.
-//        recyclerView.setLayoutManager(layoutManager);   // Also StaggeredGridLayoutManager and GridLayoutManager or a custom Layout manager
-//        adapter = new MyRecyclerViewAdapter();
-//        adapter.setData(bookList);
-//        recyclerView.setAdapter(adapter);
-
         // onClick listener for the fab button
         fabAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,6 +105,54 @@ public class bookManagementActivity extends AppCompatActivity {
                 Book book = new Book(editID.getText().toString(), editTitle.getText().toString(), editISBN.getText().toString(),
                         editDescription.getText().toString(), editAuthor.getText().toString(), Integer.parseInt(editPrice.getText().toString()));
                 mBookViewModel.insert(book);
+            }
+        });
+
+        // make the layout accessible with motion event
+        motionLayout.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int motion = motionEvent.getActionMasked();
+                int x_end, y_end;
+                int threshold = 100; // for diagonal detection
+
+                switch(motion) {
+                    case (MotionEvent.ACTION_DOWN):
+                        x_start = (int) motionEvent.getX();
+                        y_start = (int) motionEvent.getY();
+                        return true;
+
+                    case (MotionEvent.ACTION_MOVE):
+                        x_end = (int) motionEvent.getX();
+                        y_end = (int) motionEvent.getY();
+                        // left to right increasing x, so subtraction of the x end from the xstart then left to right
+                        // check if there is a big increase in Y value (like diagonal movement) then do not detect it
+                        if(Math.abs(x_end - x_start) > Math.abs(y_end - y_start) &&
+                                Math.abs(x_end - x_start - y_end + y_start) > threshold) { // Horizontal move
+                            if(x_start > x_end) { // Right to Left swipe
+                                Toast.makeText(getApplicationContext(), "Item added", Toast.LENGTH_LONG).show();
+                                showToast();
+                                Book book = new Book(editID.getText().toString(), editTitle.getText().toString(), editISBN.getText().toString(),
+                                        editDescription.getText().toString(), editAuthor.getText().toString(), Integer.parseInt(editPrice.getText().toString()));
+                                mBookViewModel.insert(book);
+                            } else { // Left to Right swipe
+                                editPrice.setText(String.valueOf(Integer.parseInt(editPrice.getText().toString()) + 1));
+                            }
+                        }
+                        return true;
+                    case (MotionEvent.ACTION_UP):
+                        x_end = (int) motionEvent.getX();
+                        y_end = (int) motionEvent.getY();
+                        if(y_start > y_end && Math.abs(x_end - x_start) < Math.abs(y_end - y_start) &&
+                                Math.abs(x_end - x_start - y_end + y_start) > threshold) { // Bottom to Top swipe
+                            clearInputs();
+                            Toast.makeText(getApplicationContext(), "Cleared Fields", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    default:
+                        return false;
+                }
             }
         });
 
@@ -258,14 +288,11 @@ public class bookManagementActivity extends AppCompatActivity {
         String Description = editDescription.getText().toString();
         int Price = Integer.parseInt(editPrice.getText().toString());
 
-//        Book book = new Book(ID, Title, ISBN, Author, Description, editPrice.getText().toString());
-
         String toastMessage = "Confirm with the title and price: \n" + "Title: " + Title + "\n" + "Price: " + Price;
 
         // All the information (everything about the application) is contained in "this"
         // LENGTH_SHORT means displaying the toast for a short period of time
         Toast.makeText(bookManagementActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
-//        bookList.add(book);
 
         SharedPreferences bookData = getSharedPreferences("last_book", 0);
         SharedPreferences.Editor bookEditor = bookData.edit();
@@ -294,13 +321,6 @@ public class bookManagementActivity extends AppCompatActivity {
 
         Toast.makeText(bookManagementActivity.this, "Cleared Inputs", Toast.LENGTH_SHORT).show();
     }
-
-//    public void doublePrice(View view) {
-//        EditText editPrice = findViewById(R.id.editPrice);
-//        int price = Integer.parseInt(editPrice.getText().toString()) * 2;
-//        Toast.makeText(bookManagementActivity.this, "Doubled the price: " + price, Toast.LENGTH_SHORT).show();
-//        editPrice.setText(String.valueOf(price));
-//    }
 
     public void loadBook() {
         SharedPreferences bookData = getSharedPreferences("last_book", 0);
@@ -354,5 +374,4 @@ public class bookManagementActivity extends AppCompatActivity {
             editPrice.setText(Price);
         }
     };
-
 }
