2ba51a9cf403ce5f4ea4e7165d340940ff8a76e7
daikidev111 <dkub0001@student.monash.edu>
2023-05-26 02:59:29 +0800

    added login/registration functionality

--- app/src/main/java/com/example/android_app/RegisterActivity.java
+++ app/src/main/java/com/example/android_app/RegisterActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,51 +16,44 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
     private EditText editTextEmailAddress, editTextPassword;
     public FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         editTextEmailAddress = findViewById(R.id.editTextEmailAddress);
         editTextPassword = findViewById(R.id.editTextPassword);
-        Button buttonSignIn = findViewById(R.id.buttonSignIn);
-        Button buttonRegistrationPage = findViewById(R.id.buttonRegistrationPage);
-        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+        Button buttonRegister = findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (editTextEmailAddress.getText().length() != 0 && editTextPassword.getText().length() != 0) {
+                if (editTextEmailAddress.getText().length() != 0 && editTextPassword.getText().length() > 6) {
                     String email = editTextEmailAddress.getText().toString();
                     String password = editTextPassword.getText().toString();
-                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 user = mAuth.getCurrentUser();
-                                Intent myIntent = new Intent(MainActivity.this, bookManagementActivity.class);
-                                startActivity(myIntent);
+                                if (user != null) {
+                                    Intent myIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                    startActivity(myIntent);
+                                }
                             } else {
-                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                Toast.makeText(RegisterActivity.this, "Authentication failed.",
                                         Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
                 } else {
-                    Toast.makeText(MainActivity.this,
-                            "Email and Password cannot be empty",
+                    Toast.makeText(RegisterActivity.this,
+                            "Email cannot be empty and password must be more than 6 characters",
                             Toast.LENGTH_SHORT
                     ).show();
                 }
-            }
-        });
-
-        buttonRegistrationPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(myIntent);
             }
         });
     }
--- app/src/main/java/com/example/android_app/UnitInterface.java
+++
@@ -1,6 +0,0 @@
-package com.example.android_app;
-
-public interface UnitInterface {
-    public void showName();
-    public void showCode();
-}
